26f543e1444ee1b3faf37edecbcdce0a157dc1f2
Adrian Haase <adrian.hasse1@web.de>
2018-03-12 19:42:42 +0100

    added functional http.get method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+export interface UserEntries {
+  id: number;
+  EMail: string;
+  Title: string;
+  Content: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<UserEntries>('http://localhost:3000/guestbook').subscribe(data => console.log(data),
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('Client-side error occured.');
+        } else {
+          console.log('Server-side error occured.');
+        }
+      }
+    );
+  }
 }
